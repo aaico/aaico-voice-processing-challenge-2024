1e8f4e254b46c59c843bc3fc90fa47d674badbe6
Suhail <suhailz13ahmed@outlook.com>
2024-02-06 12:51:20 +0400

    cooked

--- aaico_voice_processing_challenge.py
+++ aaico_voice_processing_challenge.py
@@ -6,8 +6,6 @@ import queue
 import pickle
 import pvporcupine
 
-import pygame
-
 porcupine = pvporcupine.create(
   access_key='zhpWuhGi8aOj2ssRk/jqEG1Vj96CjTaXRynJmYlgTQdC9lkPDjINbQ==',
 #   keywords=['GALACTIC-BATTERY', 'LACTIC-OXYGEN', 'LACTIC-TEMPERATURE'],
@@ -77,71 +75,33 @@ def process_data():
     i = 0
     start_event.wait()
     print('Start processing')
-    
-    
-    # pygame.mixer.pre_init(frequency=sample_rate)
-    # # Create a Pygame mixer Sound object
-    # sound = pygame.mixer.Sound(buffer=np.zeros((frame_length, 2), dtype=np.int16))
-
 
     while i != number_of_frames:
         frame = buffer.get()
         keyword_index = porcupine.process(frame)
 
-    #     # If the frame is mono, duplicate it to create stereo audio
-    #     if frame.shape[0] == frame_length:
-    #         frame2 = np.column_stack((frame, frame))
-
-    #     # Set the sound buffer by updating the array data
-    #     sound_array = pygame.sndarray.samples(sound)
-    #     sound_array[:len(frame2), :] = frame2  # Assuming stereo audio
-
-    #     # Play the audio frame using Pygame
-    #     sound.play()
-
-        ### TODO: YOUR CODE
-        # MODIFY
-
-        # print(keyword_index)
-
         if keyword_index >= 0:
-            print(keyword_index)
-            # Detected "GALACTIC" keyword
-            start_time = i * frame_length
-            while keyword_index == 0:
-                i += 1
-                frame = buffer.get()
-                keyword_index = porcupine.process(frame)
+            print(f"Detected keyword at frame {i}")
+            print(f"Detected keyword at time {i*frame_length/sample_rate}")
+            print("Length of frame: ", len(frame))
+            end_time = (i + 1) * frame_length
 
-            end_time = i * frame_length
+            start_time = max(0, end_time - 17952)
+            print(f"Start time: {start_time}")
+            print(f"End time: {end_time}")
+            
             list_samples_id = np.arange(start_time, end_time)
-            labels = [1 for _ in range(len(list_samples_id))]
-            label_samples(list_samples_id, labels)
+            labels_keyword = [0 for _ in range(len(list_samples_id))]
+            label_samples(list_samples_id, labels_keyword)
 
-            # Calculate the duration of the detected keyword interval
-            keyword_duration = (end_time - start_time) / sample_rate  # in seconds
-
-            # Make the window size dynamic based on the keyword duration
-            window_size_factor = 0.2  # You can adjust this factor based on your needs
-            window_size = int(keyword_duration * window_size_factor * sample_rate)
-
-            # Mark a window around the detected keyword as 0
-            start_window = max(0, start_time - window_size)
-            end_window = min(end_time + window_size, len(audio_data_int16))
-            list_samples_id_window = np.arange(start_window, end_window)
-            labels_window = [0 for _ in range(len(list_samples_id_window))]
-            label_samples(list_samples_id_window, labels_window)
 
         else:
             list_samples_id = np.arange(i * frame_length, (i + 1) * frame_length)
-            labels = [1 for _ in range(len(list_samples_id))]
-            label_samples(list_samples_id, labels)
+            labels_no_keyword = [1 for _ in range(len(list_samples_id))]
+            label_samples(list_samples_id, labels_no_keyword)
 
         i += 1
-
     print('Stop processing')
-
-    
     # Save the list to a file
     with open('results.pkl', 'wb') as file:
         pickle.dump(results, file)
@@ -152,8 +112,6 @@ if __name__ == "__main__":
 
     thread_process = threading.Thread(target=process_data)
     thread_emit = threading.Thread(target=emit_data)
-
-    pygame.mixer.init()
     
     thread_process.start()
     thread_emit.start()
